391e84ef8985bd1a537c61c3712f6c83bf332f85
Dilan <dunver33@gmail.com>
2021-03-02 16:42:42 +0300

    increasing and decreasing price

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const products = [
-  {name : 'mocha', img : 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/mocha-001-8301418.jpg', stok : 14, price : 10},
-  {name : 'latte', img : 'https://www.caffesociety.co.uk/assets/recipe-images/latte-small.jpg', stok : 18, price : 19},
-  {name : 'turkCoffee', img : 'https://www.theguideistanbul.com/wp-content/uploads/2018/10/38737621_1003563516492694_4277427688945221632_n-e1539950613399.jpg', stok : 12, price : 7},
-  {name : 'cappucino', img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Cappuccino_at_Sightglass_Coffee.jpg/1280px-Cappuccino_at_Sightglass_Coffee.jpg', stok : 15, price : 12}
+  {name : 'mocha', img : 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/mocha-001-8301418.jpg', stok : 14, prices : {short: 3, tall: 5, grande: 6}},
+  {name : 'latte', img : 'https://www.caffesociety.co.uk/assets/recipe-images/latte-small.jpg', stok : 18, prices : {short: 2, tall: 3, grande: 5}},
+  {name : 'turkCoffee', img : 'https://www.theguideistanbul.com/wp-content/uploads/2018/10/38737621_1003563516492694_4277427688945221632_n-e1539950613399.jpg', stok : 12, prices : {short: 5, tall: 6, grande: 8}},
+  {name : 'cappucino', img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Cappuccino_at_Sightglass_Coffee.jpg/1280px-Cappuccino_at_Sightglass_Coffee.jpg', stok : 15, prices : {short: 2.5, tall: 4, grande: 5 }}
 ];
 const selectedProducts = [];
 const productsHolder = document.querySelector('.products-holder');
@@ -19,13 +19,25 @@ function renderProducts(){
     <p>${products[i].name}</p>
     <div>
       <span>Waiting Product : ${products[i].stok}</span>
-      <span>Price : ${products[i].price} </span>
+      <span>Price : $${products[i].prices.short} </span>
     </div>
-    ${products[i].stok <= 0 ? '<span style="color: white; background-color: red;">out of stok</span><p style="color: white; background-color:blue;">cannot add any item, try other items</p>': '<button class="addToCard">Add To Card</button>'}`
+    ${products[i].stok <= 0 ? '<span style="color: white; background-color: red;">out of stok</span><p style="color: white; background-color:blue;">cannot add any item, try other items</p>': '<button class="addToCard">Add To Card</button>'}
+    <div><input type="radio" id="short" name="radAnswer" value="short">
+    <label for="short">Short</label><br>
+    <input type="radio" id="tall" name="radAnswer" value="tall">
+    <label for="tall">Tall</label><br>
+    <input type="radio" id="grande" name="radAnswer" value="grande">
+    <label for="grande">Grande</label><br></div>`
     if (products[i].stok > 0) {
       const addToCard = element.querySelector('.addToCard');
       addToCard.addEventListener('click', addProductToCard)
     }
+    const short = element.querySelector('#short');
+    short.addEventListener('click', shortItem)
+    const tall = element.querySelector('#tall');
+    tall.addEventListener('click', tallItem)
+    const grande = element.querySelector('#grande');
+    grande.addEventListener('click', grandeItem)
     
     productsHolder.appendChild(element);
   } 
@@ -42,7 +54,7 @@ function renderSelectedProducts(){
     <p>${selectedProducts[i].name}</p>
     <div>
       <span>Having Product: ${selectedProducts[i].stok}</span>
-      <span>Price : ${selectedProducts[i].price} </span>
+      <span>Price : $${selectedProducts[i].prices.short * selectedProducts[i].stok}</span>
       <button class="deleteBtn">Delete</button>
     </div>`
     const deleteBtn = element.querySelector('.deleteBtn');
@@ -83,8 +95,30 @@ function addProductToCard(e){
   renderSelectedProducts()
 }
 function deleteItem(e){
-  const element = e.currentTarget.parentElement;
-  const amount = element.dataset;
-  
-  console.log(amount)
+  const element = e.currentTarget.parentElement.parentElement;
+  const amount = element.dataset.name;
+ let findingItem = selectedProducts.find(function(item){
+    return item.name === amount;
+  })
+  let chosenItem = selectedProducts.indexOf(findingItem);
+    console.log(findingItem.stok)
+      findingItem.stok > 0 ? findingItem.stok-- : selectedProducts.splice(chosenItem,1)   
+  renderSelectedProducts()
+  renderProducts()
+  console.log(selectedProducts)
 }
+
+function shortItem(e){
+  const chosen = e.currentTarget.parentElement.parentElement;
+  const name = chosen.dataset.name
+  console.log(name)
+}
+function tallItem(e){
+  console.log('you chose tall!')
+}
+function grandeItem(e){
+  console.log('you chose grande!')
+}
+
+
+
